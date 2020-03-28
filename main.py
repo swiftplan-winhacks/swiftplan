f9452e03166cc9a36020cd0516a9a90c533edee3
Kajetan Knopp <kknopp@staszic.waw.pl>
2020-03-28 10:54:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import os, os.path, random, hashlib, sys, json
+from flask import Flask, flash, render_template, redirect, request, url_for, jsonify, session, Response
+
+app = Flask(__name__)
+app.secret_key = '9je0jaj09jk9dkakdwjnjq'
+
+@app.route('/')
+def main():
+        return redirect(url_for('index'))
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    i=0
+    print(request.form['start'])
+    while(1):
+        try:
+            print(request.form['location'+str(i)])
+            i+=1
+        except:
+            break
+    return render_template('index.html')
