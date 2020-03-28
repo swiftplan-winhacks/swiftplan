8b1ddb0ddfee8d94b23b00773c34038b20d93727
SirLemoniada <igor.dmochowski@gmail.com>
2020-03-28 23:25:59 +0100

    twoj stary

--- mapsy.py
+++ mapsy.py
@@ -4,9 +4,9 @@ import responses
 
 gmaps = googlemaps.Client(key='AIzaSyCxbvfxL6I4sScgSpwcwTiNY2D4HbGDKoA')
 
-#origins,destinations - listy krotek (lat,lng)
-#conveyance - środek transportu ('walking','driving','bycycling','transit')
-#when - moment wyjazdu (datetime)
-def howlong(origins,destinations,conveyance,when):
-    time=gmaps.distance_matrix(origins,destinations,mode=conveyance,departure_time=when, traffic_model='best_guess')
-    return time['rows'][0]['elements'][0]['duration']['value']
+
+def timeDistance(l1:Location, l2:Location,time:datatime.datatime):
+    time=gmaps.distance_matrix(l1,l2,departure_time=time, traffic_model='best_guess')
+    k=time['rows'][0]['elements'][0]['duration']['value']
+    delta=datatime.timedelta(seconds=k)
+    return delta
