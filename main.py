57a43c26d1c44c0cc2f1d37f87493e4a7aee7320
Adam Szokalski <szokalskiadam@gmail.com>
2020-03-28 19:08:37 +0100

    draft

--- main.py
+++ main.py
@@ -14,17 +14,40 @@ def main():
 def index():
     return render_template('index.html')
 
+@app.route('/addUser', methods=['POST'])
+def addUser():
+    if(request.method == "POST"):
+        #robimy uz plain texrem czemu nie
+        db.addUser(request.form['username'], request.form['password'])
+    else:
+        print("didn't add the user")
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if(request.method == "POST"):
+        #robimy uz plain texrem czemu nie
+        db.addUser(request.form['username'], request.form['password'])
+    else:
+        print("didn't log in")
+    return render_template('index.html')
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
     try:
-        print(request.form['name'])
-        print(request.form['day'])
-        print(request.form['type'])
+        t = Timeframe("01.01.1234", "13:00","02.01.1234", "13:21", "3:00")
+        e = Event(f"a{i}", "nothing", "lol", Location(00.123,00.123), True, t)
+        name = request.form['name']
+        day = request.form['day']
+        prrequest.form['type'])
         print(request.form['localization'])
         print(request.form['hour'])
         print(request.form['minute'])
         print(request.form['duration'])
+        
     except:
         pass
     return render_template('index.html')
+
+app.run('0.0.0.0', 80)
